0514352d845b641a26a15fee60341d63191aa296
Ashok Nautiyal <ashok.nautiyal@platformblack.com>
2020-02-22 00:21:31 +0000

    added docker composit yml file

--- user-service-docker-kubernetes/src/main/java/com/aitsl/user/userservice/UserServiceApplication.java
+++ user-service-docker-kubernetes/src/main/java/com/aitsl/user/userservice/UserServiceApplication.java
@@ -2,22 +2,17 @@ package com.aitsl.user.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
-
-import brave.sampler.Sampler;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
 	
-	@Bean
-	public Sampler defaultSampler() {
-		return Sampler.ALWAYS_SAMPLE;
-	}
+	/*
+	 * @Bean public Sampler defaultSampler() { return Sampler.ALWAYS_SAMPLE; }
+	 */
 
 }
